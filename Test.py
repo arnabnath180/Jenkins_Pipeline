d48e083bf8b057dae8a58d9d6f896748039d2eac
arnabnath180 <35372024+arnabnath180@users.noreply.github.com>
2023-02-20 17:40:17 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -4,7 +4,7 @@ from Prog1 import summation
 
 class TestSum(unittest.TestCase):
   def test_list_int(self):
-    data=[23,22]
+    data=[23,32]
     result=summation(data)
     self.assertEqual(result,55)
 if __name__ == '__main__':
